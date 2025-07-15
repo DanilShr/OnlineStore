df2cdf9f8adc11b049ae9b3148212806069c877e
Данил <shreder.sdan@gmai.com>
2025-07-16 00:01:50 +1100

    new

--- diploma-frontend/diploma_shop/frontend/views.py
+++ diploma-frontend/diploma_shop/frontend/views.py
@@ -1,10 +1,12 @@
 from django.views.generic import DetailView
 from .models import Product
+from .serialized import ProductSerializer
 
 
 class ProductDetailsView(DetailView):
     queryset = ((Product.objects
                  .select_related('category', 'images', 'reviews', 'specifications'))
                 .prefetch_related('tags'))
-    template_name = 'frontend/product.html'
+    serializer_class = ProductSerializer
     context_object_name = 'product'
+    template_name = 'frontend/product.html'
