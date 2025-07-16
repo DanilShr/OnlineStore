e737f52c1886c7d5bc02010da5f6f7992291d727
Данил <shreder.sdan@gmai.com>
2025-07-16 23:19:25 +1100

    new

--- diploma-frontend/diploma_shop/frontend/urls.py
+++ diploma-frontend/diploma_shop/frontend/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
+
+
+from django.urls import path, include
 from django.views.generic import TemplateView
 from .views import ProductDetailsView
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'product', ProductDetailsView)
+
 
 urlpatterns = [
+    path('api/', include(router.urls)),
+
     path('', TemplateView.as_view(template_name="frontend/index.html")),
     path('about/', TemplateView.as_view(template_name="frontend/about.html")),
     path('cart/', TemplateView.as_view(template_name="frontend/cart.html")),
@@ -13,7 +22,7 @@ urlpatterns = [
     path('orders/<int:id>/', TemplateView.as_view(template_name="frontend/order.html")),
     path('payment/<int:id>/', TemplateView.as_view(template_name="frontend/payment.html")),
     path('payment-someone/', TemplateView.as_view(template_name="frontend/paymentsomeone.html")),
-    path('product/<int:pk>/', ProductDetailsView.as_view(), name='product_details'),
+    path('product/<int:pk>/', TemplateView.as_view(template_name="frontend/product.html")),
     path('profile/', TemplateView.as_view(template_name="frontend/profile.html")),
     path('progress-payment/', TemplateView.as_view(template_name="frontend/progressPayment.html")),
     path('sale/', TemplateView.as_view(template_name="frontend/sale.html")),
--- diploma-frontend/diploma_shop/frontend/views.py
+++ diploma-frontend/diploma_shop/frontend/views.py
@@ -1,12 +1,12 @@
 from django.views.generic import DetailView
+from rest_framework.viewsets import ModelViewSet
+
 from .models import Product
 from .serialized import ProductSerializer
 
 
-class ProductDetailsView(DetailView):
+class ProductDetailsView(ModelViewSet):
     queryset = ((Product.objects
                  .select_related('category', 'images', 'reviews', 'specifications'))
                 .prefetch_related('tags'))
     serializer_class = ProductSerializer
-    context_object_name = 'product'
-    template_name = 'frontend/product.html'
